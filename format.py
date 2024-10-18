dc62ec38d7fc094c9c0014be057d94e6b7d626ae
Anugrah-Chandran <anugrahchandran2028@cs.sjcetpalai.ac.in>
2024-10-18 16:31:47 +0530

    Add files via upload

---
+++ format.py
@@ -0,0 +1,2 @@
+
+print("Twinkle,\t twinkle, \tlittle star,\n\t\tHow I wonder what you are!\n\t\t\t\t Up above the world so high,\n\t\t\t\t Like a diamond in the sky.\n Twinkle,\t twinkle,\t little star,\n\t\t How I wonder what you are")
